c3547f9e0a957d1141ba7336fd0ebf455f386315
andjela24 <1andjela.ljubenkovic@gmail.com>
2024-09-17 15:41:48 +0200

    finished all figures

--- app.js
+++ app.js
@@ -81,7 +81,6 @@ function createBoard() {
     square.firstChild?.setAttribute("draggable", true);
     square.setAttribute("square-id", i);
 
-    //Bojenje polja
     const row = Math.floor((63 - i) / 8) + 1;
     if (row % 2 === 0) {
       square.classList.add(i % 2 === 0 ? "green" : "brown");
@@ -101,7 +100,6 @@ function createBoard() {
 }
 createBoard();
 
-//Pomeranje figura
 const allSquares = document.querySelectorAll(".square");
 
 allSquares.forEach((square) => {
@@ -120,37 +118,44 @@ function dragStart(e) {
 function dragOver(e) {
   e.preventDefault();
 }
+
 function dragDrop(e) {
   e.stopPropagation();
 
-  //Provera da ispravan igrac igra
   const correctGo = draggedElement.firstChild.classList.contains(playerGo);
   const taken = e.target.classList.contains("piece");
   const valid = checkIfValid(e.target);
   const opponentGo = playerGo === "white" ? "black" : "white";
   const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo);
 
-  if (correctGo) {
-    //prva provera da li je ispravan igrac
-    if (takenByOpponent && valid) {
-      e.target.parentNode.append(draggedElement);
-      e.target.remove();
-      changePlayer();
-      return;
-    }
+  if (!correctGo) {
+    infoDisplay.textContent = "Nije tvoj red!";
+    setTimeout(() => (infoDisplay.textContent = ""), 2000);
+    return;
   }
-  //druga provera da li je polje zauzeto
+
+  if (valid && takenByOpponent) {
+    e.target.parentNode.append(draggedElement);
+    e.target.remove();
+    checkForWin();
+    changePlayer();
+    return;
+  }
+
+  if (valid) {
+    e.target.append(draggedElement);
+    checkForWin();
+    changePlayer();
+    return;
+  }
+
   if (taken && !takenByOpponent) {
     infoDisplay.textContent = "Ne možeš ovde postaviti figuru!";
     setTimeout(() => (infoDisplay.textContent = ""), 2000);
     return;
   }
-  if (valid) {
-    e.target.append(draggedElement);
-    changePlayer();
-    return;
-  }
 }
+
 function changePlayer() {
   if (playerGo === "black") {
     reverseIds();
@@ -162,18 +167,21 @@ function changePlayer() {
     player.textContent = "crni";
   }
 }
+
 function reverseIds() {
   const allSquares = document.querySelectorAll(".square");
   allSquares.forEach((square, i) => {
     square.setAttribute("square-id", 63 - i);
   });
 }
+
 function revertId() {
   const allSquares = document.querySelectorAll(".square");
   allSquares.forEach((square, i) => {
     square.setAttribute("square-id", i);
   });
 }
+
 function checkIfValid(target) {
   const targetId = Number(
     target.getAttribute("square-id") ||
@@ -181,18 +189,205 @@ function checkIfValid(target) {
   );
   const startId = Number(startPositionId);
   const piece = draggedElement.id;
-  console.log("target id", targetId);
-  console.log("start id", startId);
-  console.log("piece", piece);
+  // console.log("target id", targetId);
+  // console.log("start id", startId);
+  // console.log("piece", piece);
 
   switch (piece) {
     case "pawn":
-      const starterRow = [8, 9, 10, 11, 12, 13, 14, 15];
+      return isValidPawnMove(startId, targetId);
+    case "knight":
+      return isValidKnightMove(startId, targetId);
+    case "bishop":
+      return isValidBishopMove(startId, targetId);
+    case "rook":
+      return isValidRookMove(startId, targetId);
+    case "queen":
+      return isValidQueenMove(startId, targetId);
+    case "king":
+      return isValidKingMove(startId, targetId);
+    default:
+      return false;
+  }
+}
+
+function isValidPawnMove(startId, targetId) {
+  const starterRow = [8, 9, 10, 11, 12, 13, 14, 15];
+  if (
+    (starterRow.includes(startId) && startId + width * 2 === targetId) ||
+    startId + width === targetId ||
+    (startId + width - 1 === targetId &&
+      document.querySelector(`[square-id="${startId + width - 1}"]`)
+        .firstChild) ||
+    (startId + width + 1 === targetId &&
+      document.querySelector(`[square-id="${startId + width + 1}"]`).firstChild)
+  ) {
+    return true;
+  }
+
+  return false;
+}
+function isValidBishopMove(startId, targetId) {
+  const directions = [width + 1, width - 1, -width + 1, -width - 1];
+
+  for (const direction of directions) {
+    let currentId = startId;
+
+    while (true) {
+      currentId += direction;
+
+      if (currentId < 0 || currentId >= 64) break;
+
       if (
-        (starterRow.includes(startId) && startId + width * 2 === targetId) ||
-        startId + width === targetId
-      ) {
+        Math.abs((currentId % width) - ((currentId - direction) % width)) !== 1
+      )
+        break;
+
+      if (currentId === targetId) {
         return true;
       }
+
+      const currentSquare = document.querySelector(
+        `[square-id="${currentId}"]`
+      );
+      if (currentSquare.firstChild) break;
+    }
+  }
+
+  return false;
+}
+
+function isValidKnightMove(startId, targetId) {
+  const possibleMoves = [
+    startId + width * 2 - 1,
+    startId + width * 2 + 1,
+    startId - width * 2 - 1,
+    startId - width * 2 + 1,
+    startId + width - 2,
+    startId + width + 2,
+    startId - width - 2,
+    startId - width + 2,
+  ];
+  return possibleMoves.includes(targetId);
+}
+function isValidRookMove(startId, targetId) {
+  const directions = [1, -1, width, -width];
+  for (const direction of directions) {
+    let currentId = startId;
+
+    while (true) {
+      currentId += direction;
+
+      if (currentId < 0 || currentId >= 64) break;
+
+      if (
+        (direction === 1 || direction === -1) &&
+        Math.floor(currentId / width) !==
+          Math.floor((currentId - direction) / width)
+      )
+        break;
+
+      if (currentId === targetId) {
+        return true;
+      }
+
+      const currentSquare = document.querySelector(
+        `[square-id="${currentId}"]`
+      );
+      if (currentSquare.firstChild) break;
+    }
+  }
+
+  return false;
+}
+function isValidQueenMove(startId, targetId) {
+  const rookDirections = [1, -1, width, -width];
+  for (const direction of rookDirections) {
+    let currentId = startId;
+
+    while (true) {
+      currentId += direction;
+
+      if (currentId < 0 || currentId >= 64) break;
+
+      if (
+        (direction === 1 || direction === -1) &&
+        Math.floor(currentId / width) !==
+          Math.floor((currentId - direction) / width)
+      )
+        break;
+
+      if (currentId === targetId) {
+        return true;
+      }
+
+      const currentSquare = document.querySelector(
+        `[square-id="${currentId}"]`
+      );
+      if (currentSquare.firstChild) break;
+    }
+  }
+
+  const bishopDirections = [width + 1, width - 1, -width + 1, -width - 1];
+  for (const direction of bishopDirections) {
+    let currentId = startId;
+
+    while (true) {
+      currentId += direction;
+
+      if (currentId < 0 || currentId >= 64) break;
+
+      if (
+        Math.abs((currentId % width) - ((currentId - direction) % width)) !== 1
+      )
+        break;
+
+      if (currentId === targetId) {
+        return true;
+      }
+
+      const currentSquare = document.querySelector(
+        `[square-id="${currentId}"]`
+      );
+      if (currentSquare.firstChild) break;
+    }
+  }
+
+  return false;
+}
+function isValidKingMove(startId, targetId) {
+  const possibleMoves = [
+    startId + 1,
+    startId - 1,
+    startId + width,
+    startId - width,
+    startId + width + 1,
+    startId + width - 1,
+    startId - width + 1,
+    startId - width - 1,
+  ];
+
+  if (possibleMoves.includes(targetId)) {
+    return true;
+  }
+
+  return false;
+}
+function checkForWin() {
+  const kings = Array.from(document.querySelectorAll("#king"));
+  console.log("Kings", kings);
+  if (!kings.some((king) => king.firstChild.classList.contains("white"))) {
+    infoDisplay.innerHTML = "Crni igrač je pobedio!";
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square) =>
+      square.firstChild?.setAttribute("draggable", false)
+    );
+  }
+  if (!kings.some((king) => king.firstChild.classList.contains("black"))) {
+    infoDisplay.innerHTML = "Beli igrač je pobedio!";
+    const allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((square) =>
+      square.firstChild?.setAttribute("draggable", false)
+    );
   }
 }
